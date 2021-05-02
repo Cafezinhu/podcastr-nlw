13fec7817e243d5832fada8032a8fd934a7b4403
Aerton <aertoncastro@gmail.com>
2021-05-02 09:57:36 -0300

    :construction: PWA

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,9 @@ export default class MyDocument extends Document{
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet" />
                     <link rel="shortcut icon" href="/favicon.png" type="image/x-icon"/>
+                    <meta name='theme-color' content='#fff' />
+                    <link rel='apple-touch-icon' href='/favicon.png' />
+                    <link rel='manifest' href='/manifest.json' />
                 </Head>
                 <body>
                     <Main />
